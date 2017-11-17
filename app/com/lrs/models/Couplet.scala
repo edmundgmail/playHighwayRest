4efb6a50e3a99e528c36784586d5c52c0b76377a
eguo <eguo@localhost.localdomain>
2017-11-17 16:33:34 -0500

    add new models

---
+++ app/com/lrs/models/Couplet.scala
@@ -0,0 +1,21 @@
+package com.lrs.models
+
+import com.lrs.daos.core.TemporalModel
+import play.api.libs.json.Json
+import reactivemongo.bson.BSONObjectID
+import org.joda.time.DateTime
+
+
+/**
+  * Created by eguo on 11/17/17.
+  */
+case class Couplet(
+                    var _id: Option[BSONObjectID] = None,
+                    var created: Option[DateTime] = None,
+                    var updated: Option[DateTime] = None) extends TemporalModel
+
+object Couplet{
+  import reactivemongo.play.json.BSONFormats.BSONObjectIDFormat // This is required
+  implicit def coupletFormat = Json.format[Couplet]
+
+}
---
+++ app/com/lrs/models/Ramp.scala
@@ -0,0 +1,20 @@
+package com.lrs.models
+
+import com.lrs.daos.core.TemporalModel
+import org.joda.time.DateTime
+import play.api.libs.json.Json
+import reactivemongo.bson.BSONObjectID
+
+/**
+  * Created by eguo on 11/17/17.
+  */
+case class Ramp(
+                 var _id: Option[BSONObjectID] = None,
+                 var created: Option[DateTime] = None,
+                 var updated: Option[DateTime] = None) extends TemporalModel
+
+object Ramp{
+  import reactivemongo.play.json.BSONFormats.BSONObjectIDFormat // This is required
+  implicit def rampFormat = Json.format[Ramp]
+
+}

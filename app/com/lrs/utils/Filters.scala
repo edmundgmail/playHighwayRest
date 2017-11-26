27eac504c18364fcb5b6cd92a309f7166a5c1f70
eguo <eguo@localhost.localdomain>
2017-11-26 18:33:02 -0500

    update

--- app/com/lrs/utils/Filters.scala
+++ app/com/lrs/utils/Filters.scala
@@ -8,5 +8,5 @@ import javax.inject.Inject
 import play.api.http.DefaultHttpFilters
 import play.filters.cors.CORSFilter
 
-class MyFilters @Inject() (corsFilter: CORSFilter)
+class Filters @Inject() (corsFilter: CORSFilter)
   extends DefaultHttpFilters(corsFilter)
